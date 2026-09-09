0c03be539d7d8d9f9a67186abf63747232e060a2
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2026-09-09 20:59:55 +0200

    Render library document keys consistently

--- green/test/clj/io/github/getcolors/signoz/workflow_test.clj
+++ green/test/clj/io/github/getcolors/signoz/workflow_test.clj
@@ -13,3 +13,7 @@
  (is (= "legacy compute state requires migration" (:green/err (compute/attach (fixture) {:status "error" :errors ["legacy compute state requires migration"]}))))
  (is (= "ubuntu" (:user (compute/attach (fixture) {:status "present" :cluster {:nodes [{:ip "203.0.113.7" :user "ubuntu"}]}}))))
  (is (:signoz/already-destroyed (compute/attach (fixture) {:status "destroyed"}))))
+
+(deftest library-document-keys-render-deterministically
+ (is (= (#'io.github.getcolors.signoz.compute/compute-json {"a" 0 :b 1} 0)
+        (#'io.github.getcolors.signoz.compute/compute-json {:a 0 "b" 1} 0))))
